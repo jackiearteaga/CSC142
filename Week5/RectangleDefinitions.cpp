b184db179c8ef69fb010bf085caaa9f9f63bc01e
Jackie Arteaga <guadalupe.arteaga001@albright.edu>
2025-02-27 00:40:52 -0500

    Update RectangleDefinitions.cpp

--- Week5/RectangleDefinitions.cpp
+++ Week5/RectangleDefinitions.cpp
@@ -1,1 +1,24 @@
+// RectangleDefinitions.cpp
+// Week 5 Task 2
+// Name: Jackie Arteaga
+#include "Rectangle.h"
 
+double Rectangle::getWidth() const {
+    return width;
+}
+
+double Rectangle::getLength() const {
+    return length;
+}
+
+void Rectangle::setWidth(double w) {
+    width = w;
+}
+
+void Rectangle::setLength(double l) {
+    length = l;
+}
+
+double Rectangle::getArea() const {
+    return length * width;
+}
