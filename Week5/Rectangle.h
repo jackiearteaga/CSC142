35809d53faf03b01f03c96d47e7085e77e848332
Jackie Arteaga <guadalupe.arteaga001@albright.edu>
2025-02-27 00:39:55 -0500

    Update Rectangle.h

--- Week5/Rectangle.h
+++ Week5/Rectangle.h
@@ -1,1 +1,26 @@
+// Rectangle.h
+// Week 5 Task 2
+// Name: Jackie Arteaga
+#ifndef RECTANGLE_H
+#define RECTANGLE_H
 
+class Rectangle {
+private:
+    double length;  //rectangle length
+    double width;   //rectangle width
+
+public:
+    Rectangle() : length(0.0), width(0.0) {}
+
+    double getWidth() const;
+
+    double getLength() const;
+
+    void setWidth(double w);
+
+    void setLength(double l);
+
+    double getArea() const;
+};
+
+#endif
